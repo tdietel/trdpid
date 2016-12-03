9018bc85160cd03dd9542fb7b10a6e1f844d12fe
Tom Dietel <tom@dietel.net>
2016-12-03 08:28:00 +0200

    started refactoring AliTRDrawData to AliTRDdigitsFilter

---
+++ filter/filter.C
@@ -0,0 +1,68 @@
+void ana()
+{
+  // for includes use either global setting in $HOME/.rootrc
+  // ACLiC.IncludePaths: -I$(ALICE_ROOT)/include
+  // or in each macro
+  gSystem->AddIncludePath("-I$ALICE_ROOT/include");
+
+  gSystem->Load("libTree.so");
+  gSystem->Load("libGeom.so");
+  gSystem->Load("libVMC.so");
+  gSystem->Load("libSTEERBase.so");
+  gSystem->Load("libESD.so");
+  gSystem->Load("libAOD.so"); 
+
+  // load analysis framework
+  gSystem->Load("libANALYSIS");
+  gSystem->Load("libANALYSISalice");
+
+  gROOT->LoadMacro("CreateESDChain.C");
+  TChain* chain = CreateESDChain("files.txt", 1, 0, kFALSE, kTRUE);
+
+
+  // Create the analysis manager
+  AliAnalysisManager *mgr = new AliAnalysisManager("testAnalysis");
+
+
+
+  AliESDInputHandler* esdH = new AliESDInputHandler();
+  mgr->SetInputEventHandler(esdH);
+
+  // Enable MC event handler
+  AliMCEventHandler* handler = new AliMCEventHandler;
+  handler->SetReadTR(kFALSE);
+  mgr->SetMCtruthEventHandler(handler);
+
+
+  
+  // Create digits filter (df) task
+
+  gROOT->LoadMacro("AliTRDPIDrawData.cxx+g");
+  AliAnalysisTask *taskdf = new AliTRDPIDrawData("PIDrawData");
+
+  // Add task(s)
+  mgr->AddTask(taskdf);
+
+  // Create containers for input/output
+  AliAnalysisDataContainer *cinput = mgr->GetCommonInputContainer();
+  AliAnalysisDataContainer *coutputdf =
+    mgr->CreateContainer("cdigflt", TList::Class(),   
+			 AliAnalysisManager::kOutputContainer,
+			 "DigitsFilter.root");
+
+  // Connect input/output
+  mgr->ConnectInput(taskdf, 0, cinput);
+
+  // No need to connect to a common AOD output container if the task does not
+  // fill AOD info.
+  //  mgr->ConnectOutput(task, 0, coutput0);
+  mgr->ConnectOutput(taskdf, 1, coutputdf);
+  cout << "done" << endl;
+  
+  // Enable debug printouts
+  mgr->SetDebugLevel(2);
+
+  if (!mgr->InitAnalysis()) return;
+  mgr->PrintStatus();
+  mgr->StartAnalysis("local", chain);
+}
